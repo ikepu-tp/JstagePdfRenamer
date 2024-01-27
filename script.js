1884923da3667ef8551ac4a71cd18cda2104432e
ikepu-tp <postmaster@licsss.jp>
2024-01-27 23:28:25 +0900

    Refactor script.js to handle missing elements and provide default values for author names, paper title, publication date, and PDF URL.

--- script.js
+++ script.js
@@ -2,20 +2,33 @@ if (
   !window.location.href.match(/https:\/\/www.jstage.jst.go.jp\/article\/[^\S]*/)
 )
   throw new Error("Invalid URL");
+
 try {
+  var pdf_url_element = document.getElementsByName("pdf_url");
+  if (!pdf_url_element[0]) throw new Error("PDF URL not found");
+
   var authors_element = document.getElementsByName("authors");
   var authors = [];
   for (var i = 0; i < authors_element.length; ++i) {
-    var author_name = authors_element[i].content;
+    var author_name = authors_element[i]
+      ? authors_element[i].content
+      : "Unknown";
     var splited_author_name = author_name.split(" ");
     if (i > 2) break;
     authors.push(splited_author_name[0] + (i === 2 ? "ら" : ""));
   }
 
-  var paper_title = document.getElementsByName("title")[0].content;
-  var paper_publication_date =
-    document.getElementsByName("publication_date")[0].content;
-  var pdf_url = document.getElementsByName("pdf_url")[0].content;
+  var paper_title_element = document.getElementsByName("title");
+  var paper_publication_date_element =
+    document.getElementsByName("publication_date");
+
+  var paper_title = paper_title_element[0]
+    ? paper_title_element[0].content
+    : "Unknown";
+  var paper_publication_date = paper_publication_date_element[0]
+    ? new Date(paper_publication_date_element[0].content).getFullYear()
+    : "Unknown";
+  var pdf_url = pdf_url_element[0].content;
 
   var file_name =
     authors.join("・") +
