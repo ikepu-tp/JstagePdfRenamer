e4f5d44f70a8343a5db9d14cc9c2ee2a29272498
ikepu-tp <postmaster@licsss.jp>
2025-03-22 21:02:09 +0900

    upd for public

--- vite.config.ts
+++ vite.config.ts
@@ -11,4 +11,11 @@ export default defineConfig({
     },
     port: 3000,
   },
+  build: {
+    lib: {
+      entry: ["src/main.tsx", "src/addElements.tsx"],
+      fileName: (format, entryName) => `${entryName}.${format}.js`,
+      formats: ["es", "cjs"],
+    },
+  },
 });
